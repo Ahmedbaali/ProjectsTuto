9cc4d4ff60c63515b14c77da899d3e2057a06e66
Ahmed BAALI <baali.ahmed7@gmail.com>
2023-08-04 23:09:11 +0100

    continue with api scraper

--- ScrapeAPI/Scrape_api.py
+++ ScrapeAPI/Scrape_api.py
@@ -8,4 +8,23 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 
+BASE_URL = 'https://remoteok.com/api/'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0'
+#this agnet is for this PC(Linux Manjaro) for other system I have to change the agent
+#can use this site 'https://www.whatismybrowser.com/detect/what-is-my-user-agent/'
+REQUEST_HEADER = {
+    'User-Agent': USER_AGENT,
+    'Accept-Language': 'en-US, en;q=0.5'
+}
 
+def get_job_posting():
+    res = requests.get(url=BASE_URL, headers=REQUEST_HEADER)
+    return res.json()
+
+
+
+
+
+if __name__ == "__main__":
+    json = get_job_posting()[1:]
+    print(json)
